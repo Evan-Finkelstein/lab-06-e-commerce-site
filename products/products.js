177312c976f3f431a3985bbd56da712e0d2d98e0
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-10-05 14:26:00 -0700

    initial commit

---
+++ products/products.js
@@ -0,0 +1,8 @@
+export const product = {
+    id: 'soup-for-my-family',
+    name: 'soup-for-my-family',
+    image: 'soup-for-my-family.png',
+    description: 'Soup to feed your family!',
+    category: 'food',
+    price: 13.12,
+};
